33e9a22f6d86c688b57f2b4d48de3ec4ed41c508
sumitkrravi <141992643+sumitkrravi@users.noreply.github.com>
2025-04-15 02:41:34 +0530

    Update AggregateStatsTest.java

--- wrangler-core/src/test/java/io/cdap/wrangler/directives/AggregateStatsTest.java
+++ wrangler-core/src/test/java/io/cdap/wrangler/directives/AggregateStatsTest.java
@@ -1,1 +1,18 @@
 
+package io.cdap.wrangler.directives;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AggregateStatsTest {
+
+    @Test
+    public void testSum() {
+        int[] numbers = {1, 2, 3, 4};
+        int sum = 0;
+        for (int n : numbers) {
+            sum += n;
+        }
+        Assert.assertEquals(10, sum);
+    }
+}
